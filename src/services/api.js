232ce2ced168576435f16e081dd7df554dacbc8a
Gabriel Koch Fodi <gabrielkochfodi@gmail.com>
2021-09-18 00:11:22 -0300

    changing url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000'
+  baseURL: process.env.API_ENDPOINT || 'localhost:3000'
 })
 
 export default api
+// https://pets-backend-iulan.ondigitalocean.app'
