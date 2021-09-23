988db35c59fbe939466aec4b9b2caf4043096d3f
Gabriel Koch Fodi <67654630+gabrikf@users.noreply.github.com>
2021-09-23 13:26:14 -0300

    Update Users.js

--- src/pages/Users.js
+++ src/pages/Users.js
@@ -89,8 +89,8 @@ const Users = () => {
                     <input className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring dark:text-white" 
                     type="text" 
                     name='name'
-                    placeholder="Digite seu nome completo" 
-                    aria-label="Nome completo"
+                    placeholder="Digite o nome da ONG ou seu nome completo" 
+                    aria-label="Nome da ONG ou seu nome completo"
                     value={formik.values.name}
                     onChange={formik.handleChange}
                     
