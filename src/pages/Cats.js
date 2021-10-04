597c7ec99241a4fbb85eff8696dd6b1c7abc3b79
Gabriel Koch Fodi <gabrielkochfodi@gmail.com>
2021-10-03 22:04:03 -0300

    default name when dont have one seted

--- src/pages/Cats.js
+++ src/pages/Cats.js
@@ -161,7 +161,7 @@ const Cats = () => {
                   <GiCat className="text-white" />
                 )}
                 <h1 className="mx-3 text-lg font-semibold text-white">
-                  {incident.pet_name}
+                  {incident.pet_name.length > 0 ? incident.pet_name : 'Sem nome até o momento'}
                 </h1>
               </div>
             </Link>
